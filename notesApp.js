82f8369a3d8a1af09920f7de5868dad6605eac0b
MacMillon-VS <macsimson3@gmail.com>
2023-03-29 07:14:18 +0530

    Formatted

--- notesApp.js
+++ notesApp.js
@@ -1,10 +1,10 @@
-const addTitle = document.getElementById('addTitle');
-const addText = document.getElementById('addText');
-const addNoteButton = document.getElementById('addNote');
-const notesDiv = document.getElementById('notes');
+const addTitle = document.getElementById("addTitle");
+const addText = document.getElementById("addText");
+const addNoteButton = document.getElementById("addNote");
+const notesDiv = document.getElementById("notes");
 
-const deletedDiv = document.getElementById('deleted-items');
-const archivedDiv = document.getElementById('archived-items');
+const deletedDiv = document.getElementById("deleted-items");
+const archivedDiv = document.getElementById("archived-items");
 
 let searchBar = document.getElementById("search-bar");
 
@@ -12,196 +12,188 @@ showNotes();
 // local storage vs session storage
 // JSON: JavaScript Object Notation
 
-function addNotes(){
-    // let notesA=[]
-    let notes = localStorage.getItem('notes');
-    if(notes === null){
-        notes = [];
-    }else{
-        notes = JSON.parse(notes);
-    }
+function addNotes() {
+  // let notesA=[]
+  let notes = localStorage.getItem("notes");
+  if (notes === null) {
+    notes = [];
+  } else {
+    notes = JSON.parse(notes);
+  }
 
-    if(addText.value == ''){
-        alert('Add your note');
-        return;
-    }
-    
-    const noteObj = {
-        title: addTitle.value,
-        text: addText.value,
-    }
-    addTitle.value = '';
-    addText.value = '';
-    notes.push(noteObj);
-    localStorage.setItem('notes', JSON.stringify(notes));
-    showNotes();
+  if (addText.value == "") {
+    alert("Add your note");
+    return;
+  }
+
+  const noteObj = {
+    title: addTitle.value,
+    text: addText.value,
+  };
+  addTitle.value = "";
+  addText.value = "";
+  notes.push(noteObj);
+  localStorage.setItem("notes", JSON.stringify(notes));
+  showNotes();
 }
 
-function showNotes(){
-    let notesHTML = '';
-    let notes = localStorage.getItem('notes');
-    if(notes === null){
-        return;
-    }else{
-        notes = JSON.parse(notes);
-    }
-    for(let i=0; i<notes.length; i++){
-        notesHTML += `<div class="note" id="note-list">
+function showNotes() {
+  let notesHTML = "";
+  let notes = localStorage.getItem("notes");
+  if (notes === null) {
+    return;
+  } else {
+    notes = JSON.parse(notes);
+  }
+  for (let i = 0; i < notes.length; i++) {
+    notesHTML += `<div class="note" id="note-list">
                     <button class="deleteNote" id=${i} onclick="deleteNote(${i})">Delete</button>
-                    <span class="title">${notes[i].title === "" ? 'Note' : notes[i].title}</span>
+                    <span class="title">${
+                      notes[i].title === "" ? "Note" : notes[i].title
+                    }</span>
                     <div class="text">${notes[i].text}</div>
                 </div>
-        `
-    }
-    notesDiv.innerHTML = notesHTML;
+        `;
+  }
+  notesDiv.innerHTML = notesHTML;
 }
 
-let deletedNotes =[];
-function deleteNote(ind){
-    let notes = localStorage.getItem('notes');
-    if(notes === null){
-        return;
-    }else{
-        notes = JSON.parse(notes);
-    }
-    
-    deletedNotes.push(notes[ind]);
-    console.log(deletedNotes);
-    localStorage.setItem('deletedNotes',JSON.stringify(deletedNotes));
-    let  notesHTML='' ;
-    let dNotes = localStorage.getItem('deletedNotes');
-    console.log(dNotes);
-    if(dNotes === null){
-        return;
-    }
-    else{
-        dNotes = JSON.parse(dNotes);
-    }
-    for(let i=0; i<dNotes.length; i++){
+let deletedNotes = [];
+function deleteNote(ind) {
+  let notes = localStorage.getItem("notes");
+  if (notes === null) {
+    return;
+  } else {
+    notes = JSON.parse(notes);
+  }
+
+  deletedNotes.push(notes[ind]);
+  console.log(deletedNotes);
+  localStorage.setItem("deletedNotes", JSON.stringify(deletedNotes));
+  let notesHTML = "";
+  let dNotes = localStorage.getItem("deletedNotes");
+  console.log(dNotes);
+  if (dNotes === null) {
+    return;
+  } else {
+    dNotes = JSON.parse(dNotes);
+  }
+  for (let i = 0; i < dNotes.length; i++) {
     notesHTML += `<div class="note" id="note-list">
     <button class="deleteNote" id=${i} onclick="retriveNote(${i})">Retrive</button>
     <button class="deleteNote" id=${i} onclick="deleteNotePermanent(${i})">Delete</button>
     <span class="title">${dNotes[i].title}</span>
     <div class="text">${dNotes[i].text}</div>
 </div>
-`
-    }
-deletedDiv.innerHTML=notesHTML;
-archivedDiv.innerHTML=notesHTML;
+`;
+  }
+  deletedDiv.innerHTML = notesHTML;
+  archivedDiv.innerHTML = notesHTML;
 
-    notes.splice(ind, 1);
-    localStorage.setItem('notes', JSON.stringify(notes));
-    showNotes();
-    updateDeleteNotes();
+  notes.splice(ind, 1);
+  localStorage.setItem("notes", JSON.stringify(notes));
+  showNotes();
+  updateDeleteNotes();
 }
 
-function updateDeleteNotes(){
-    let notesHTML = '';
-    
-    let dNotes = localStorage.getItem('deletedNotes');
-    if(dNotes === null){
-        return;
-    }else{
-        dNotes = JSON.parse(dNotes);
-    }
-    for(let i=0; i<dNotes.length; i++){
+function updateDeleteNotes() {
+  let notesHTML = "";
+
+  let dNotes = localStorage.getItem("deletedNotes");
+  if (dNotes === null) {
+    return;
+  } else {
+    dNotes = JSON.parse(dNotes);
+  }
+  for (let i = 0; i < dNotes.length; i++) {
     notesHTML += `<div class="note" id="note-list">
     <button class="deleteNote" id=${i} onclick="retriveNote(${i})">Retrive</button>
     <button class="deleteNote" id=${i} onclick="deleteNotePermanent(${i})">Delete</button>
     <span class="title">${dNotes[i].title}</span>
     <div class="text">${dNotes[i].text}</div>
 </div>
-`
-    }
-deletedDiv.innerHTML=notesHTML;
-archivedDiv.innerHTML=notesHTML;
-
+`;
+  }
+  deletedDiv.innerHTML = notesHTML;
+  archivedDiv.innerHTML = notesHTML;
 }
 
-function deleteNotePermanent(ind){
-    let dNotes = localStorage.getItem('deletedNotes');
-    console.log(dNotes);
-    if(dNotes === null){
-        return;
-    }else{
-        dNotes = JSON.parse(dNotes);
-    }
-    dNotes.splice(ind, 1);
-    localStorage.setItem('deletedNotes', JSON.stringify(dNotes));
-    showNotes();
-    updateDeleteNotes();
+function deleteNotePermanent(ind) {
+  let dNotes = localStorage.getItem("deletedNotes");
+  console.log(dNotes);
+  if (dNotes === null) {
+    return;
+  } else {
+    dNotes = JSON.parse(dNotes);
+  }
+  dNotes.splice(ind, 1);
+  localStorage.setItem("deletedNotes", JSON.stringify(dNotes));
+  showNotes();
+  updateDeleteNotes();
 }
 
-function retriveNote(ind){
-    let dNotes = localStorage.getItem('deletedNotes');
-    let notes = localStorage.getItem('notes');
-    if(dNotes === null){
-        return;
-    }else{
-        dNotes = JSON.parse(dNotes);
-    }
-    if(notes === null){
-        notes = [];
-    }else{
-        notes = JSON.parse(notes);
-    }
+function retriveNote(ind) {
+  let dNotes = localStorage.getItem("deletedNotes");
+  let notes = localStorage.getItem("notes");
+  if (dNotes === null) {
+    return;
+  } else {
+    dNotes = JSON.parse(dNotes);
+  }
+  if (notes === null) {
+    notes = [];
+  } else {
+    notes = JSON.parse(notes);
+  }
 
-    dNotes.slice(ind,1);
-    notes.push(dNotes[ind]);
-    localStorage.setItem('notes', JSON.stringify(notes));
-    localStorage.setItem('deletedNotes', JSON.stringify(dNotes));
-    showNotes();
-    updateDeleteNotes();
+  dNotes.slice(ind, 1);
+  notes.push(dNotes[ind]);
+  localStorage.setItem("notes", JSON.stringify(notes));
+  localStorage.setItem("deletedNotes", JSON.stringify(dNotes));
+  showNotes();
+  updateDeleteNotes();
 }
 
-
-addNoteButton.addEventListener('click', addNotes);
-
-
+addNoteButton.addEventListener("click", addNotes);
 
 function toggleCard(id) {
-    let cardBody = document.getElementById(id);
-    let icon = cardBody.previousElementSibling.querySelector('i');
-    
-    if (cardBody.style.display === 'none') {
-      cardBody.style.display = 'block';
-    } else {
-      cardBody.style.display = 'none';
-    }
+  let cardBody = document.getElementById(id);
+  let icon = cardBody.previousElementSibling.querySelector("i");
+
+  if (cardBody.style.display === "none") {
+    cardBody.style.display = "block";
+  } else {
+    cardBody.style.display = "none";
   }
+}
 
+let notesA = JSON.parse(localStorage.getItem("notes")) || [];
 
+let noteList = document.getElementById("note-list");
 
-  let notesA = JSON.parse(localStorage.getItem('notes')) || []; // Get notes from local storage
-
-  let noteList = document.getElementById('note-list');
-  
-  // Add event listener to search bar
-  searchBar.addEventListener('input', function() {
-    let searchQuery = searchBar.value.toLowerCase(); // Get search query
-    
-    // Filter notes and update note list
-    let filteredNotes = notesA.filter(function(note) {
-      return note.title.toLowerCase().includes(searchQuery) || note.text.toLowerCase().includes(searchQuery);
-    });
-    
-    updateNoteList(filteredNotes); // Update note list
+searchBar.addEventListener("input", function () {
+  let searchQuery = searchBar.value.toLowerCase();
+  let filteredNotes = notesA.filter(function (note) {
+    return (
+      note.title.toLowerCase().includes(searchQuery) ||
+      note.text.toLowerCase().includes(searchQuery)
+    );
   });
-  
-  // Update note list function
-  function updateNoteList(notes) {
-    notesDiv.innerHTML = ''; // Clear note list
-    // Iterate through filtered notes and add them to note list
-    notes.forEach(function(note) {
-        
-      let noteCard = document.createElement('div');
-      noteCard.classList.add('note');
-      noteCard.innerHTML = `<h2>  ${note.title === "" ? 'Note' : note.title}  </h2>  <p>  ${note.text}  </p>`;
-      notesDiv.appendChild(noteCard);
-    });
-  }
-  
 
+  updateNoteList(filteredNotes);
+});
+
+function updateNoteList(notes) {
+  notesDiv.innerHTML = "";
+  notes.forEach(function (note) {
+    let noteCard = document.createElement("div");
+    noteCard.classList.add("note");
+    noteCard.innerHTML = `<h2>  ${
+      note.title === "" ? "Note" : note.title
+    }  </h2>  <p>  ${note.text}  </p>`;
+    notesDiv.appendChild(noteCard);
+  });
+}
 
 // assignment
 
